b7f0fb618e2aca0d4bc893dceffd4d83d67db288
Keks <keks@htmlacademy.ru>
2015-09-26 23:27:16 +0300

    :cat: Обновления от Кекса для модуля «Препроцессоры, часть 1»

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,37 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   var config = {
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      style: {
+        files: {
+          'css/style.css': 'less/style.less'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({browsers: 'last 2 versions'})
+        ]
+      },
+      style: {
+        src: 'css/*.css'
+      }
+    },
+
+    watch: {
+      style: {
+        files: ['less/**/*.less'],
+        tasks: ['less', 'postcss'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
   };
 
   config = require('./.gosha')(grunt, config);
